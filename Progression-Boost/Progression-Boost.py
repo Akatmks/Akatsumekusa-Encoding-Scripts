61e35b22531b9d83a52aa4e9cff7a1aeba711ea9
Akatsumekusa <Akatsumekusa@protonmail.com>
2025-08-12 16:39:30 +0000

    Preset: Fix thanks to pratama

--- Progression-Boost/Progression-Boost.py
+++ Progression-Boost/Progression-Boost.py
@@ -917,28 +917,28 @@ class DefaultZone:
     
         return mean
 # For Butteraugli 3Norm score, uncomment the lines below.
-    def metric_summarise(self, scores: np.ndarray[np.float32]) -> np.float32:
-        # mean = np.mean(scores ** 3) ** (1 / 3)
-        #
-        # median = np.median(scores)
-        # mad = np.median(np.abs(scores - median))
-        #
-        # limit = median + mad * 3.0
-        # overlimit = np.max(scores) - limit
-        # for score_i in range(scores.shape[0] - 5 + 1):
-        #     i_limit = np.median(scores[score_i:score_i + 5]) + mad * 3.0
-        #     i_overlimit = np.max(scores[score_i:score_i + 5]) - i_limit
-        #     overlimit = np.max([overlimit, i_overlimit])
-        # if overlimit < 0:
-        #     overlimit = 0
-        #
-        # if verbose >= 2:
-        #     if overlimit > 0:
-        #         print(f"\r\033[K{scene_frame_print(scene_n)} / Metric summarisation / mean {mean:.3f} / adjusted mean {mean + overlimit:.3f}", end="\n")
-        #
-        # mean += overlimit
-        #
-        # return mean
+    # def metric_summarise(self, scores: np.ndarray[np.float32]) -> np.float32:
+    #     mean = np.mean(scores ** 3) ** (1 / 3)
+    #
+    #     median = np.median(scores)
+    #     mad = np.median(np.abs(scores - median))
+    #
+    #     limit = median + mad * 3.0
+    #     overlimit = np.max(scores) - limit
+    #     for score_i in range(scores.shape[0] - 5 + 1):
+    #         i_limit = np.median(scores[score_i:score_i + 5]) + mad * 3.0
+    #         i_overlimit = np.max(scores[score_i:score_i + 5]) - i_limit
+    #         overlimit = np.max([overlimit, i_overlimit])
+    #     if overlimit < 0:
+    #         overlimit = 0
+    #
+    #     if verbose >= 2:
+    #         if overlimit > 0:
+    #             print(f"\r\033[K{scene_frame_print(scene_n)} / Metric summarisation / mean {mean:.3f} / adjusted mean {mean + overlimit:.3f}", end="\n")
+    #
+    #     mean += overlimit
+    #
+    #     return mean
 
 # If you want to use a different method than above to summarise the
 # data, implement your own method here.
