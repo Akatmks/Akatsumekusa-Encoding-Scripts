8d585e7abb9015960ad874200591d601142c673a
Akatsumekusa <Akatsumekusa@protonmail.com>
2025-09-21 05:56:26 +0000

    Progression Boost: Fix

--- Progression-Boost/Progression-Boost.py
+++ Progression-Boost/Progression-Boost.py
@@ -1865,6 +1865,8 @@ if not resume or not scene_detection_scenes_file.exists():
             diffs_0012 = diffs >= 0.0012
             diffs_0048 = diffs >= 0.0048
             surrounding_diffs = np.maximum(diffs[:-2], diffs[2:])
+            diffs[0] -= diffs[1]
+            diffs[-1] -= diffs[-2]
             diffs[1:-1] -= surrounding_diffs
             diffs[diffs < diffs_half] = diffs_half
             diffs[np.logical_and(diffs_0012, diffs < 0.0012)] = 0.0012
